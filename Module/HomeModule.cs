6f0a2a79a6209e35620ecad17d174f587560bbb3
lawton browning <lawton.a.browning@gmail.com>
2016-03-04 13:23:12 -0800

    add homemodule and index

---
+++ Module/HomeModule.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+using ShoeStore;
+
+namespace ShoeStore
+{
+  public class HomeModule: NancyModule
+  {
+
+    public HomeModule()
+    {
+
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
